ee9cfd9cb6ca560fa599f42442bd0400ba98f018
Mevlüde Saniye ORTAÇ <mevludeortac@users.noreply.github.com>
2021-05-30 18:14:05 +0300

    Initial Commit

---
+++ alert/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  alert
+//
+//  Created by MEWO on 30.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
